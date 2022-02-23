9d5aad1a7c7959eb3471906384bd5a4dce448468
James Goldie <james.goldie@360info.org>
2022-02-23 17:07:24 +1100

    Annotations working (geom_textbox still WIP)

--- R/labels.r
+++ R/labels.r
@@ -1,4 +1,89 @@
-#' Add a 360info-themed textbox to the plot.
+# #' Add a 360info-themed textbox geom to the plot.
+# #'
+# #' @param text_colour A string for the text colour
+# #' @param ... Other arguments passed on to `ggtext::geom_textbox` or
+# #'   `ggplot2::geom_label`.
+# #' @importFrom rlang warn
+# #' @importFrom cli format_warning
+# #' @importFrom ggplot2 unit geom_label
+# #' @export
+# geom_textbox360 <- function(
+#   text_colour = c("dark", "light", "blue"),
+#   box_colour = c(NA_character_, names(colours_360())),
+#   size = 7, ...) {
+
+#   text_colour <- match.arg(text_colour)
+#   box_colour <- match.arg(box_colour)
+
+#   text_colour <- switch(text_colour,
+#     "dark" = colours_360("black"),
+#     "light" = colours_360("white"),
+#     "blue" = colours_360("lightblue"))
+#   if (!is.na(box_colour)) {
+#     box_colour <- colours_360(box_colour)
+#   }
+
+#   ggtext::geom_textbox(
+#     fill = box_colour, colour = text_colour,
+#     family = "Body 360info",
+#     size = 7,
+#     box.r = unit(0, "cm"), box.padding = unit(0.5,  "cm"),
+#     box.colour = NA,
+#     ...)
+# }
+
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_light <- function(...) {
+#   geom_textbox360(text_colour = "dark", box_colour = "lightgrey", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_dark <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "darkgrey", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_lightblue <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "lightblue", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_darkblue <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "darkblue", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_teal <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "teal", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_green <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "green", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_lightgrey <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "lightgrey", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_darkgrey <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = "darkgrey", ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_glasslight <- function(...) {
+#   geom_textbox360(text_colour = "dark", box_colour = NA_character_, ...)
+# }
+# #' @rdname geom_textbox360
+# #' @export
+# geom_textbox360_glassdark <- function(...) {
+#   geom_textbox360(text_colour = "light", box_colour = NA_character_, ...)
+# }
+
+#' Add a 360info-themed textbox annotation to the plot.
 #'
 #' @param box_style A string for one of the pre-defined 360info styles. Affects
 #'   the fill and text colour of the box. One of "light", "dark", "blue",
@@ -9,55 +94,66 @@
 #' @importFrom cli format_warning
 #' @importFrom ggplot2 unit geom_label
 #' @export
-geom_label360 <- function(
-  box_style = c("light", "dark", "blue", "glass_light", "glass_dark"),
-  ...) {
+annotate_360 <- function(
+  text_colour = c("dark", "light", "blue"),
+  box_colour = c(NA_character_, names(colours_360())),
+  size = 7, ...) {
 
-  # determine colours from selected pre-fill style
-  box_style <- match.arg(box_style)
-  all_styles <- data.frame(
-    style = c("light", "dark", "blue", "glass_light", "glass_dark"),
-    fill = c("white", "black", "#36a7e9", NA, NA),
-    colour = c("black", "white", "white", "black", "white"))
-  selected_style <- all_styles[all_styles$style == box_style, ]
+  text_colour <- match.arg(text_colour)
+  box_colour <- match.arg(box_colour)
 
-  # return either a geom_textbox (if available) or a geom_label (if not)
-  if (requireNamespace("ggtext", quietly = TRUE)) {
-
-    ggtext::geom_textbox(
-      fill = selected_style$fill, colour = selected_style$colour,
-      family = "Body 360info",
-      size = 5,
-      box.r = unit(0, "cm"), box.padding = unit(0.5,  "cm"),
-      box.colour = NA,
-    ...)
-
-  } else {
-
-    # throw a warning (once) if ggtext isn't installed...
-    warn(
-      format_warning(c(
-        "i" = paste(
-          "The {.pkg ggtext} package isn't installed. This means that textbox",
-          "features like {.emph formatting text} and line wrapping aren't",
-          "available."),
-        ">" = paste(
-          "Get it by running the command: {.code install.packages('ggtext')},",
-          "then try again to enable these features."))),
-      .frequency = "once", .frequency_id = "ggtext_unavailable")
-    
-    # ... then proceed with a regular textbox
-    geom_label(
-      fill = selected_style$fill, colour = selected_style$colour,
-      family = "Body 360info",
-      size = 5,
-      label.r = unit(0, "cm"), label.padding = unit(0.5,  "cm"),
-      label.colour = NA,
-      ...)
-
+  text_colour <- switch(text_colour,
+    "dark" = colours_360("black"),
+    "light" = colours_360("white"),
+    "blue" = colours_360("lightblue"))
+  if (!is.na(box_colour)) {
+    box_colour <- colours_360(box_colour)
   }
 
-  
+  annotate(geom = "richtext", colour = text_colour, fill = box_colour,
+    label.colour = NA, family = "Body 360info", size = size,
+    label.r = unit(0, "cm"), label.padding = unit(0.25, "cm"),
+    ...)
 }
 
-# TODO - annotate_360, using geom = `ggtext:geom_textbox`?
+#' @export
+annotate_360_light <- function(...) {
+  annotate_360(text_colour = "dark", box_colour = "lightgrey", ...)
+}
+#' @export
+annotate_360_dark <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "darkgrey", ...)
+}
+#' @export
+annotate_360_lightblue <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "lightblue", ...)
+}
+#' @export
+annotate_360_darkblue <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "darkblue", ...)
+}
+#' @export
+annotate_360_teal <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "teal", ...)
+}
+#' @export
+annotate_360_green <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "green", ...)
+}
+#' @export
+annotate_360_lightgrey <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "lightgrey", ...)
+}
+#' @export
+annotate_360_darkgrey <- function(...) {
+  annotate_360(text_colour = "light", box_colour = "darkgrey", ...)
+}
+#' @export
+annotate_360_glasslight <- function(...) {
+  annotate_360(text_colour = "dark", box_colour = NA_character_, ...)
+}
+#' @export
+annotate_360_glassdark <- function(...) {
+  annotate_360(text_colour = "light", box_colour = NA_character_, ...)
+}
+
