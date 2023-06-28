059bb6b979d25ba9353ad052b6da0b114b0da58e
James Goldie <james.goldie@360info.org>
2023-06-28 10:10:17 +1000

    Switch to Public Sans

--- R/fonts.r
+++ R/fonts.r
@@ -1,28 +1,8 @@
 #' Returns a CSS `@import` declaration for the 360 fonts. Used by `save_plot`
 #' when saving SVG files so that other users viewing them on the web can see
 #' 360info fonts without having them installed.
-#'
-#' @importFrom svglite font_face
-#' @importFrom rlang warn
-#' @importFrom cli format_warning cli_bullets
 fontfaces_360fonts <- function() {
-
-  loaded_font_name <- getOption("themes360info.franklin.loaded")
-
-  if (is.null(loaded_font_name)) {
-    abort(format_warning(cli_bullets(c(
-      "x" = paste0(
-        "The 360info font wasn't successfully loaded, so the package ",
-        "can't write font information to the SVG file.",
-      "i" = "Install the Libre Franklin font on your system and reload.")
-    ))))
-  }
-
-  loaded_font_spec <- switch(loaded_font_name,
-    "libre" = list(paste0(
-      "https://fonts.googleapis.com/css2?family=Libre+Franklin:",
-      "ital,wght@0,400;0,700;0,900;1,400&display=swap")),
-    list())
-
-  return(loaded_font_spec)
+  list(paste0(
+    "https://fonts.googleapis.com/css2?family=Public+Sans:",
+    "ital,wght@0,400;0,700;0,900;1,400&display=swap"))
 }
--- R/zzz.r
+++ R/zzz.r
@@ -1,20 +1,25 @@
+#' On package startup, register the ttf fils in inst/extdata
+#' as "Headline 360info", "Subhead 360info" and "Body 360info"
+#' 
+#' (Note that the subhead font is no longer used by the theme
+#' but is left for backwards-compatibility purposes.)
 .onLoad <- function(libname, pkgname) {
   systemfonts::register_font(
     name = "Headline 360info",
-    plain = system.file("extdata", "LibreFranklin-Black.ttf",
+    plain = system.file("extdata", "PublicSans-Black.ttf",
       package = "themes360info"))
   systemfonts::register_font(
     name = "Subhead 360info",
-    plain = system.file("extdata", "LibreFranklin-Bold.ttf",
+    plain = system.file("extdata", "PublicSans-Bold.ttf",
       package = "themes360info"))
   systemfonts::register_font(
     name = "Body 360info",
-    plain = system.file("extdata", "LibreFranklin-Light.ttf",
+    plain = system.file("extdata", "PublicSans-Regular.ttf",
       package = "themes360info"), 
-    italic = system.file("extdata", "LibreFranklin-LightItalic.ttf",
+    italic = system.file("extdata", "PublicSans-Italic.ttf",
       package = "themes360info"),
-    bold = system.file("extdata", "LibreFranklin-Bold.ttf",
+    bold = system.file("extdata", "PublicSans-Bold.ttf",
       package = "themes360info"),
-    bolditalic = system.file("extdata", "LibreFranklin-BoldItalic.ttf",
+    bolditalic = system.file("extdata", "PublicSans-BoldItalic.ttf",
       package = "themes360info"))
 }
